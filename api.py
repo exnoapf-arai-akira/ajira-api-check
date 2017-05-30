3f282e6358f663e265c79ec3cebf011276780680
tsuchinoko <ar@dooga.co.jp>
2017-05-30 20:09:40 +0900

    modified api.py

--- api.py
+++ api.py
@@ -6,35 +6,50 @@ import numpy as np
 import json
 import urllib2
 import os
+import urllib
 
-f = open('urllist2.txt')
 
-for url in f:
-    list = url.split("/")
-    filename = list[-1]
-    print url
-    cmd = "wget %s" % (url)
-    os.system(cmd)
+def url_to_image(url):
+	resp = urllib.urlopen(url)
+	image = np.asarray(bytearray(resp.read()), dtype="uint8")
+	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+	return image
 
-    img = cv2.imread(filename)
+urls = open('urllist2.txt')
+
+#for url in f:
+#    print url
+#    urllist = url.split("/")
+#    filename = urllist[-1]
+#    cmd = "wget -N %s" % (url)
+#    os.system(cmd)
+
+
+for url in urls:
+    urllist = url.split("/")
+    filename = urllist[-1]
+    print "downloading %s" % (url)
+    img = url_to_image(url)
 
     aurl = 'http://52.38.2.70:5000/api/maria?url='+str(url)
 
     r = urllib2.urlopen(aurl)
     jsonData = json.loads(r.read())
     r.close()
-    ##print json.dumps(jsonData, sort_keys=True, indent=4)
+    print json.dumps(jsonData, sort_keys=True, indent=4)
+
+    #img = cv2.imread(filename)
 
     for list in jsonData['detect']:
         cv2.rectangle(img,(list['right'],list['top']),(list['left'],list['bottom']),(0,255,0),3)
         cv2.putText(img,list['label'],(list['left'],list['top']-10),cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 0, 255),2)
 
-##cv2.imshow('result', img)
-##cv2.waitKey(0)
-##cv2.destroyAllWindows()
+    #cv2.imshow('result', img)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
     cv2.imwrite("detect-"+filename, img)
 
 
-f.close()
+urls.close()
 ### right,top,left,bottom
