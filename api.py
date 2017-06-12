147c5e63554d9585bf1fb95fc3b2158f24e56744
labotsuchinoko <ar@dooga.co.jp>
2017-06-12 14:47:03 +0900

    modified api.py

--- api.py
+++ api.py
@@ -7,7 +7,19 @@ import json
 import urllib2
 import os
 import urllib
+import sys
+import StringIO
 
+def url_or_file(text):
+    if "http" in text:
+        urls = StringIO.StringIO(text)
+        return urls
+    else:
+        urls = open(text)
+        return urls
+
+#if __name__ == "__main__":
+#    url_or_file(sys.argv[1])
 
 def url_to_image(url):
 	resp = urllib.urlopen(url)
@@ -15,15 +27,13 @@ def url_to_image(url):
 	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 	return image
 
-urls = open('urllist2.txt')
-
-for url in urls:
+for url in url_or_file(sys.argv[1]):
     urllist = url.split("/")
     filename = urllist[-1]
     print "downloading %s" % (url)
     img = url_to_image(url)
 
-    aurl = 'http://52.38.2.70:5000/api/maria?url='+str(url)
+    aurl = 'http://104.199.154.201:5000/api/maria?url='+str(url)
 
     r = urllib2.urlopen(aurl)
     jsonData = json.loads(r.read())
@@ -34,7 +44,7 @@ for url in urls:
 
     for list in jsonData['detect']:
         cv2.rectangle(img,(list['right'],list['top']),(list['left'],list['bottom']),(0,255,0),3)
-        cv2.putText(img,list['label'],(list['left'],list['top']-10),cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 0, 255),2)
+        cv2.putText(img,list['class'],(list['left'],list['top']-10),cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 0, 255),2)
 
     #cv2.imshow('result', img)
     #cv2.waitKey(0)
@@ -42,6 +52,6 @@ for url in urls:
 
     cv2.imwrite("detect-"+filename, img)
 
-
-urls.close()
+if url_or_file(sys.argv[1]):
+    url_or_file(sys.argv[1]).close()
 ### right,top,left,bottom
